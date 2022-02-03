7d43c47276466fad5601faf6f5cb4388157e7af4
aleix <a.casamitjana@ultramarmanagement.com>
2022-02-03 15:39:02 +0100

    Hacer que se puedan editar solo las entradas de un mismo usuario.

--- blog/tables.py
+++ blog/tables.py
@@ -9,15 +9,16 @@ class PublishedDateColumn(tables.DateTimeColumn):
 
     def render(self, value, *args, **kwargs):
         val = super().render(value=value, *args, **kwargs)
-        return format_html('<div class="date">{}</div>', val)
+        return format_html('<div class="date">{}</div>', val) #o template_name..
 
 class PersonTable(tables.Table):
     title = columns.LinkColumn("post_detail", args=[A("pk")])
     published_date = PublishedDateColumn()
+    options = tables.TemplateColumn(template_name='blog/option_list.html')
 
     class Meta:
         model = Post
-        sequence = ("title", "text", "author", "published_date")
+        sequence = ("title", "text", "author", "published_date", "options",)
         exclude = ("id", "created_date")
 
     def render_text(self, value):
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.shortcuts import redirect
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib import messages
 from django_tables2 import SingleTableView
 from django_tables2.export.views import ExportMixin
 
@@ -31,7 +34,7 @@ class DetailPostView(generic.DetailView):
     template_name = "blog/post_detail.html"
 
 
-class CreatePostView(generic.CreateView):
+class CreatePostView(LoginRequiredMixin, generic.CreateView):
     template_name = "blog/post_edit.html"
     form_class = PostForm
     model = Post
@@ -47,14 +50,14 @@ class CreatePostView(generic.CreateView):
         return reverse("post_detail", kwargs={"pk": self.object.pk})
 
 
-class EditPostView(generic.UpdateView):
+class EditPostView(LoginRequiredMixin, generic.UpdateView):
     template_name = "blog/post_edit.html"
     form_class = PostForm
     model = Post
 
     def form_valid(self, form):
         post = form.save(commit=False)
-        post.author = self.request.user
+        # post.author = self.request.user # ya no hace falta modificarlo, porque tiene que ser el mismo autor
         post.published_date = timezone.now()
         post.save()
         return super().form_valid(form)
@@ -62,10 +65,22 @@ class EditPostView(generic.UpdateView):
     def get_success_url(self):
         return reverse("post_detail", kwargs={"pk": self.object.pk})
 
+    def dispatch(self, request, *args, **kwargs):
+        if self.get_object().author != self.request.user:
+            messages.error(self.request, "You are not allowed to edit this entry!")
+            return HttpResponseRedirect(reverse('post_list'))
+        return super().dispatch(request, *args, **kwargs)
 
-class RemovePostView(generic.DeleteView):
+
+class RemovePostView(LoginRequiredMixin, generic.DeleteView):
     model = Post
     form = PostForm
 
     def get_success_url(self):
         return reverse("post_list")
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.get_object().author != self.request.user:
+            messages.error(self.request, "You are not allowed to delete this entry!")
+            return HttpResponseRedirect(reverse('post_list'))
+        return super().dispatch(request, *args, **kwargs)
